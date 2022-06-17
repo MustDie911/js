64929a6eb9903d8b19861e79c2da20b60068f585
ArtemKozak <a.s.kozak@ukr.net>
2022-06-17 20:12:06 +0300

    lesson 12//test

--- lesson 12/lesson 12.js
+++ lesson 12/lesson 12.js
@@ -27,20 +27,20 @@
 //     Для кожного елементу свій блок div.comment
 // Всі характеристики повинні мати свої блоки всередені div.comment
 
-fetch('https://jsonplaceholder.typicode.com/comments')
-    .then(value => value.json())
-    .then(comments => {
-        let divFullComments = document.createElement('div');
-        divFullComments.classList.add('divFullComments');
-        for (const divFullComment of comments) {
-            let htmlDivElement = document.createElement('div');
-            htmlDivElement.classList.add('htmlDivElement');
-            htmlDivElement.innerHTML = `<p>${divFullComment.postId}</p>
-<p><strong>id:</strong> ${divFullComment.id}</p>
-<p><strong>name:</strong> ${divFullComment.name}</p>
-<p><strong>email:</strong> ${divFullComment.email}</p>
-<h2><I>'${divFullComment.body}'</I></h2>`
-            divFullComments.append(htmlDivElement);
-            document.body.append(divFullComments);
-        }
-    })
+// fetch('https://jsonplaceholder.typicode.com/comments')
+//     .then(value => value.json())
+//     .then(comments => {
+//         let divFullComments = document.createElement('div');
+//         divFullComments.classList.add('divFullComments');
+//         for (const divFullComment of comments) {
+//             let htmlDivElement = document.createElement('div');
+//             htmlDivElement.classList.add('htmlDivElement');
+//             htmlDivElement.innerHTML = `<p>${divFullComment.postId}</p>
+// <p><strong>id:</strong> ${divFullComment.id}</p>
+// <p><strong>name:</strong> ${divFullComment.name}</p>
+// <p><strong>email:</strong> ${divFullComment.email}</p>
+// <h2><I>'${divFullComment.body}'</I></h2>`
+//             divFullComments.append(htmlDivElement);
+//             document.body.append(divFullComments);
+//         }
+//     })
